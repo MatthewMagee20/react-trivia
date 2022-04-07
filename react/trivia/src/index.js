a96cb87e8859ec775b8ba3510d352cc627d10a38
Matthew Magee <matthewmagee@MacBook-Air.local>
2022-04-07 16:57:53 +0100

    index js routes

--- react/trivia/src/index.js
+++ react/trivia/src/index.js
@@ -9,10 +9,9 @@ import Complete from "./components/complete";
 render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="quiz" element={<Quiz />}></Route>
-        <Route path="complete" element={<Complete />}></Route>
-      </Route>
+      <Route path="/" element={<App />}></Route>
+      <Route path="quiz" element={<Quiz />}></Route>
+      <Route path="complete" element={<Complete />}></Route>
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
