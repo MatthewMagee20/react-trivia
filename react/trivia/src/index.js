8d23746bef3e67af9a97ac6d8e276ef3eb5f2b4d
Matthew Magee <matthewmagee@MacBook-Air.local>
2022-04-07 16:43:18 +0100

    index.js changes

--- react/trivia/src/index.js
+++ react/trivia/src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import App from "./App";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Quiz from "./components/quiz";
 import Complete from "./components/complete";
 
-ReactDOM.render(
+render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}></Route>
-      <Route path="quiz" element={<Quiz />}></Route>
-      <Route path="complete" element={<Complete />}></Route>
+      <Route path="/" element={<App />}>
+        <Route path="quiz" element={<Quiz />}></Route>
+        <Route path="complete" element={<Complete />}></Route>
+      </Route>
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
